745ba2744a8d07fdd473cf223fd0440d416a3e47
Victoria Mattera <vmfxdesigns@gmail.com>
2022-04-01 09:35:38 -0500

    added Training Basic Data Types--Array

--- 8kyu/basicDataTypesArray.js
+++ 8kyu/basicDataTypesArray.js
@@ -47,3 +47,14 @@ function popElement(arr){
     arr.pop();
     return arr;
 }
+
+//best practices
+const getLength = array => array.length;
+
+const getFirst = array => array[0];
+
+const getLast = array => array[array.length - 1];
+
+const pushElement = array => array.concat(0);
+
+const popElement = array => array.splice(0, array.length -1);
