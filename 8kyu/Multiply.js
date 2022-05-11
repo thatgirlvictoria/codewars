d0b08bf86666040219c435550982ad8c257fa1bd
Victoria Mattera <vmfxdesigns@gmail.com>
2022-05-10 22:07:12 -0500

    added new codewars challenge

---
+++ 8kyu/Multiply.js
@@ -0,0 +1,8 @@
+//This code does not execute properly. Try to figure out why.
+
+//My solution
+const multiply = (a, b) => {
+    return a * b;
+  };
+  let resulMultiply = multiply(4, 5);
+  console.log(resulMultiply);
