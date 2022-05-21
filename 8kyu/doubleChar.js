6cb50b26f4209d9ec653702a3d7c5a65c66f6efd
Victoria Mattera <vmfxdesigns@gmail.com>
2022-05-21 18:09:08 -0500

    added Double Char codewars

--- 8kyu/doubleChar.js
+++ 8kyu/doubleChar.js
@@ -9,3 +9,13 @@
 function doubleChar(str) {
     return str.replace(/(.)/g, "$1$1")
 }
+
+
+//best practices
+function doubleChar(str) {
+    var word = '';
+    for (var i = 0; i < str.length; i++){
+      word = word + str[i] + str[i];
+    };
+    return word;
+  };
