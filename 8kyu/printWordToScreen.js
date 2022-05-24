cbdaa0dcd365ddd9c421adae2d07d77b9214be83
Victoria Mattera <vmfxdesigns@gmail.com>
2022-05-24 13:53:00 -0500

    Training JS #1: create your first JS function and print "Hello World!"

---
+++ 8kyu/printWordToScreen.js
@@ -0,0 +1,34 @@
+/*Training JS #1: create your first JS function and print "Hello World!"
+
+If we want to print some to the screen, maybe we can use Document.write() in the web, or use alert() pop your message, but Codewars did not support these methods, we should use console.log() in your function. see this example:
+
+function printWordToScreen(){
+  var somewords="This is an example."
+  console.log(somewords)
+}
+If we run this function, This is an example. will be seen on the screen. As you see, console.log() is an useful method help your work, you will become more and more like it.
+
+Task
+Please refer to two example above and write your first JS function.
+
+mission 1:
+
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+
+use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+
+type the console.log() in the next line(don't forget to put the str in the brackets).
+
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+In the end, click "Submit" to submit your code pass this kata.
+*/
+
+function helloWorld(){
+    let helloWorld="Hello World!"
+    console.log(helloWorld)
+}
